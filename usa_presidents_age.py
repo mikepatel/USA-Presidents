20eda2240ab5142849781723bbb6678ca106e12f
Michael Patel <michael.patel94@gmail.com>
2020-06-26 13:22:14 -0400

    Got all data

--- usa_presidents_age.py
+++ usa_presidents_age.py
@@ -22,13 +22,31 @@ from multiprocessing import Pool
 ################################################################################
 # Main
 if __name__ == "__main__":
+    # get data from wikipedia
     url = "https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States_by_age"
 
-    with urllib.request.urlopen(url) as response:
-        page = response.read()
+    df = pd.read_html(io=url, header=0)
+    age_df = df[0]  # first table
 
-    page = re.sub('<!--|-->', "", str(page))
+    # parse data
+    names = []
+    ages = []
+    start_dates = []
 
-    # html soup
-    soup = BeautifulSoup(page, "html.parser")
-    
+    for index, row in age_df.iterrows():
+        name = str(row["President"])
+        age = str(row["Age atstart of presidency"])
+        if name != "President":
+            names.append(name)
+
+            age = re.sub("\xa0", " ", age)
+            print(age)
+            age, date = age.split("days")
+            age, _ = age.split(" years")
+            ages.append(age)
+            start_dates.append(date)
+
+    print(names)
+    print(ages)
+    print(start_dates)
+
